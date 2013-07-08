6aa5bdd4942ee5a32477c7a7468dab3016340c71
Enric M. Calvo <enric.calvo@gmail.com>
2013-07-08 20:25:21 +0000

    + dump contents from index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
+fs = require('fs');
+var filename = 'index.html';
+var filetext = fs.readFileSync(filename, 'utf8', function(err,data){
+    if (err) {
+        return console.log(err);
+    }
+    return data.toString('utf8');
+});
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = Buffer
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(filetext);
 });
 
 var port = process.env.PORT || 5000;
