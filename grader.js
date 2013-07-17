c350537b640e7446ec1c3d203ec3e1e66007d5f1
Enric M. Calvo <enric.calvo@gmail.com>
2013-07-17 15:43:49 +0000

    WIP: in my way to get url working

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://supeng-hw3.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,8 +67,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <check_url>', 'URL to index.html', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var urldata = rest.get(program.url).on('complete', function(data) {
+        return data;
+    });
+    var data2check = [];
+    if (program.url){
+        data2check = urldata;
+    } else {
+        data2check = program.file;
+    }
+
+    var checkJson = checkHtmlFile(data2check, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
